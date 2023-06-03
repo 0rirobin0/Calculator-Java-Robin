46a30540f40255d7de4c6138bcc4c24feeb6fff4
Robin <rirobink@gmail.com>
2023-06-03 17:06:15 +0600

    Added Component Frame Object

--- src/CalculatorApplication/CalApp.java
+++ src/CalculatorApplication/CalApp.java
@@ -1,2 +1,105 @@
-package CalculatorApplication;public class CalApp {
+package CalculatorApplication;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+
+
+
+
+public class CalApp {
+    //Swing Javaframe Class Object Declaration//
+    JFrame frame=new JFrame("Calculator-Robin");
+    JLabel label = new JLabel();
+    JTextField textField = new JTextField();
+    JRadioButton onRadioButton = new JRadioButton("on");
+    JRadioButton offRadioButton = new JRadioButton("off");
+    JButton buttonZero = new JButton("0");
+    JButton buttonOne = new JButton("1");
+    JButton buttonTwo = new JButton("2");
+    JButton buttonThree = new JButton("3");
+    JButton buttonFour = new JButton("4");
+    JButton buttonFive = new JButton("5");
+    JButton buttonSix = new JButton("6");
+    JButton buttonSeven = new JButton("7");
+    JButton buttonEight = new JButton("8");
+    JButton buttonNine = new JButton("9");
+    JButton buttonDot = new JButton(".");
+    JButton buttonClear = new JButton("C");
+    JButton buttonDelete = new JButton("DEL");
+    JButton buttonEqual = new JButton("=");
+    JButton buttonMul = new JButton("x");
+    JButton buttonDiv = new JButton("/");
+    JButton buttonPlus = new JButton("+");
+    JButton buttonMinus = new JButton("-");
+    JButton buttonSquare = new JButton("x\u00B2");
+    JButton buttonReciprocal = new JButton("1/x");
+    JButton buttonSqrt = new JButton("\u221A");
+
+
+
+
+
+
+
+
+
+
+    //UserGraphics Frame//
+
+    //frame
+    public void prepareGUI()
+    {
+        frame.setSize(305,510);
+        frame.getContentPane().setLayout(null);
+        frame.getContentPane().setBackground(Color.black);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
+    }
+    //Components
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Constructor for call methods
+    CalApp(){
+        prepareGUI();
+
+
+
+
+
+
+
+    }
+
+
+
+    //Main Class//
+    public static void main(String[] args) {
+
+        CalApp Calculator=new CalApp();
+
+
+
+
+
+
+
+    }
+
+
 }
